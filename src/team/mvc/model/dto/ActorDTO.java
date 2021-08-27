45cbb40d4a468dd3450a2522ac36ce2449cf58db
threebears00 <77993864+threebears00@users.noreply.github.com>
2021-08-27 10:47:32 +0900

    사라진 DTO 수정
    ActorDTO
    MovidDetailDTO 다시 추가

--- src/team/mvc/model/dto/ActorDTO.java
+++ src/team/mvc/model/dto/ActorDTO.java
@@ -2,4 +2,93 @@ package team.mvc.model.dto;
 
 public class ActorDTO {
 
+	
+	private int movieNo;
+	private String mainActor1;
+	private String mainActor2;
+	private String supActor1;
+	private String supActor2;
+	private String supActor3;
+	
+	public ActorDTO() {}
+
+	public ActorDTO(int movieNo, String mainActor1, String mainActor2, String supActor1, String supActor2,
+			String supActor3) {
+		super();
+		this.movieNo = movieNo;
+		this.mainActor1 = mainActor1;
+		this.mainActor2 = mainActor2;
+		this.supActor1 = supActor1;
+		this.supActor2 = supActor2;
+		this.supActor3 = supActor3;
+	}
+
+	public int getMovieNo() {
+		return movieNo;
+	}
+
+	public void setMovieNo(int movieNo) {
+		this.movieNo = movieNo;
+	}
+
+	public String getMainActor1() {
+		return mainActor1;
+	}
+
+	public void setMainActor1(String mainActor1) {
+		this.mainActor1 = mainActor1;
+	}
+
+	public String getMainActor2() {
+		return mainActor2;
+	}
+
+	public void setMainActor2(String mainActor2) {
+		this.mainActor2 = mainActor2;
+	}
+
+	public String getSupActor1() {
+		return supActor1;
+	}
+
+	public void setSupActor1(String supActor1) {
+		this.supActor1 = supActor1;
+	}
+
+	public String getSupActor2() {
+		return supActor2;
+	}
+
+	public void setSupActor2(String supActor2) {
+		this.supActor2 = supActor2;
+	}
+
+	public String getSupActor3() {
+		return supActor3;
+	}
+
+	public void setSupActor3(String supActor3) {
+		this.supActor3 = supActor3;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("ActorDTO [movieNo=");
+		builder.append(movieNo);
+		builder.append(", mainActor1=");
+		builder.append(mainActor1);
+		builder.append(", mainActor2=");
+		builder.append(mainActor2);
+		builder.append(", supActor1=");
+		builder.append(supActor1);
+		builder.append(", supActor2=");
+		builder.append(supActor2);
+		builder.append(", supActor3=");
+		builder.append(supActor3);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 }
--- src/team/mvc/model/dto/MovidDetailDTO.java
+++ src/team/mvc/model/dto/MovidDetailDTO.java
@@ -1,5 +1,95 @@
 package team.mvc.model.dto;
 
+import java.util.List;
+
 public class MovidDetailDTO {
 
+	private int movieNo;
+	private String content;
+	private int runtime;
+	private String releaseDate;
+	private String nation;
+	private List<String> userTag;
+	
+	public MovidDetailDTO() {}
+
+	public MovidDetailDTO(int movieNo, String content, int runtime, String releaseDate, String nation,
+			List<String> userTag) {
+		super();
+		this.movieNo = movieNo;
+		this.content = content;
+		this.runtime = runtime;
+		this.releaseDate = releaseDate;
+		this.nation = nation;
+		this.userTag = userTag;
+	}
+
+	public int getMovieNo() {
+		return movieNo;
+	}
+
+	public void setMovieNo(int movieNo) {
+		this.movieNo = movieNo;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public int getRuntime() {
+		return runtime;
+	}
+
+	public void setRuntime(int runtime) {
+		this.runtime = runtime;
+	}
+
+	public String getReleaseDate() {
+		return releaseDate;
+	}
+
+	public void setReleaseDate(String releaseDate) {
+		this.releaseDate = releaseDate;
+	}
+
+	public String getNation() {
+		return nation;
+	}
+
+	public void setNation(String nation) {
+		this.nation = nation;
+	}
+
+	public List<String> getUserTag() {
+		return userTag;
+	}
+
+	public void setUserTag(List<String> userTag) {
+		this.userTag = userTag;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("MovidDetailDTO [movieNo=");
+		builder.append(movieNo);
+		builder.append(", content=");
+		builder.append(content);
+		builder.append(", runtime=");
+		builder.append(runtime);
+		builder.append(", releaseDate=");
+		builder.append(releaseDate);
+		builder.append(", nation=");
+		builder.append(nation);
+		builder.append(", userTag=");
+		builder.append(userTag);
+		builder.append("]");
+		return builder.toString();
+	}
+	
+	
 }
