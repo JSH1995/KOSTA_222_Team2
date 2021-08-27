915a7a0e932fecfc97fddb10d7077a40340edc6a
june-hong <53648623+june-hong@users.noreply.github.com>
2021-08-27 10:32:49 +0900

    DTO construction
    EvaluationDTO, GenreListDTO, TagListDTO

--- src/dto/TagListDTO.java
+++ src/dto/TagListDTO.java
@@ -1,5 +1,44 @@
 package dto;
 
 public class TagListDTO {
+	private int tagNo;
+	private String tagName;
+
+	public TagListDTO() {
+
+	}
+
+	public TagListDTO(int tagNo, String tagName) {
+		super();
+		this.tagNo = tagNo;
+		this.tagName = tagName;
+	}
+
+	public int getTagNo() {
+		return tagNo;
+	}
+
+	public String getTagName() {
+		return tagName;
+	}
+
+	public void setTagNo(int tagNo) {
+		this.tagNo = tagNo;
+	}
+
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+
+	@Override
+	public String toString() {
+		StringBuilder builder = new StringBuilder();
+		builder.append("TagListDTO [tagNo=");
+		builder.append(tagNo);
+		builder.append(", tagName=");
+		builder.append(tagName);
+		builder.append("]");
+		return builder.toString();
+	}
 
 }
