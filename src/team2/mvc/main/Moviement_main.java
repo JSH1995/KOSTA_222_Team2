7d6ed921ffca7d369bceafbfaa75f67f95b61b67
JSH1995 <44968599+JSH1995@users.noreply.github.com>
2021-08-29 00:32:23 +0900

    사용자 (테스트)기능구현

--- src/team2/mvc/main/Moviement_main.java
+++ src/team2/mvc/main/Moviement_main.java
@@ -1,13 +1,14 @@
 package team2.mvc.main;
 
+import team2.mvc.view.MenuView;
+
 public class Moviement_main {
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("test");
-		System.out.println("test2");
+		MenuView.menu();
 
 	}
 
