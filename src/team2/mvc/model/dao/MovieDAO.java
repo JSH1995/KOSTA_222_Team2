286d3b8b6388ee7adabea0d908bd17d3b956966e
Cheein <88135096+Cheein@users.noreply.github.com>
2021-08-31 23:51:06 +0900

    api

--- src/team2/mvc/model/dao/MovieDAO.java
+++ src/team2/mvc/model/dao/MovieDAO.java
@@ -11,72 +11,128 @@ import team2.mvc.model.dto.Tag;
 public interface MovieDAO {
 	
 	/**
-	 *  영화 등록
+	 * 영화 등록
+	 * @author 이치인
+	 * @param movieNo
+	 * @param genreNo
+	 * @param movieName
+	 * @param director
+	 * @param date1
+	 * @throws SQLException 
+	 * 
 	 * */
 	int registerMovie(int movieNo, int genreNo, String movieName, String director, String date1) throws SQLException;
 	
 	/**
-	 *  영화 수정
+	 * 영화 수정
+	 * @author 이치인
+	 * @param movieNo
+	 * @param genreNo
+	 * @param movieName
+	 * @param director
+	 * @param date1
+	 * @throws SQLException 
 	 * */
 	int updateMovie(int movieNo, int genreNo, String movieName, String director, String date1)throws SQLException;
 	
 	/**
 	 * 영화 삭제
+	 * @author 이치인
+	 * @param movieNo
+	 * @throws SQLException 
 	 * */
 	int deleteMovie(int movieNo) throws SQLException;
 	
 	/**
-	 *  배우 등록
+	 * 배우 등록
+	 * @author 이치인
+	 * @param actor
+	 * @throws SQLException 
 	 * */
 	int registerActor(Actor actor) throws SQLException;
 	
 	/**
-	 *  배우 수정
+	 * 배우 수정
+	 * @author 이치인
+	 * @param actor
+	 * @throws SQLException 
 	 * */
 	int updateActor(Actor actor)throws SQLException;
 	
 	/**
 	 * 배우 삭제
+	 * @author 이치인
+	 * @param movieNo
+	 * @throws SQLException 
 	 * */
 	int deleteActor(int movieNo) throws SQLException;
 	
 	/**
-	 *  태그 등록
+	 * 태그 등록
+	 * @author 이치인
+	 * @param tag
+	 * @throws SQLException  
 	 * */
 	int registerTag(Tag tag) throws SQLException;
 	
 	/**
-	 *  태그 수정
+	 * 태그 수정
+	 * @author 이치인
+	 * @param tag
+	 * @throws SQLException 
 	 * */
 	int updateTag(Tag tag)throws SQLException;
 	
-	/** 
+	/**
 	 * 태그 삭제
+	 * @author 이치인
+	 * @param movieNo
+	 * @throws SQLException  
 	 * */
 	int deleteTag(int movieNo) throws SQLException;
 	
 	/**
-	 *  영화상세정보 등록
+	 * 영화상세정보 등록
+	 * @author 이치인
+	 * @param movieDetail
+	 * @throws SQLException  
 	 * */
 	int registerMovieDetail(MovidDetail movieDetail) throws SQLException;
 	
 	/**
-	 *  영화상세정보 수정
+	 * 영화상세정보 수정
+	 * @author 이치인
+	 * @param movieDetail
+	 * @throws SQLException 
 	 * */
 	int updateMovieDetail(MovidDetail movieDetail)throws SQLException;
 	
 	/**
 	 * 영화상세정보 삭제
+	 * @author 이치인
+	 * @param movieNo
+	 * @throws SQLException 
 	 * */
 	int deleteMovieDetail(int movieNo) throws SQLException;
 
-	/**
-	 * Menuview UI 에 사용할 부수적인 메소드들
-	 *     1. 영화 고유번호에 해당하는 레코드 검색
-	 * */
+    //Menuview UI 에 사용할 부수적인 메소드들
 	
+	/**
+	 * 영화고유번호에 해당하는 영화 레코드 검색
+	 * @author 이치인
+	 * @param movieNo
+	 * @return List<MovieSub>
+	 * @throws SQLException 
+	 * */
 	List<MovieSub> selectByMovieNo(int movieNo) throws SQLException;
 	
+	/**
+	 * 영화고유번호에 해당하는 태그 레코드 검색
+	 * @author 이치인
+	 * @param movieNo
+	 * @return List<Tag>
+	 * @throws SQLException 
+	 * */
 	List<Tag> tagListByMovieNo(int movieNo) throws SQLException;
 	
 	
