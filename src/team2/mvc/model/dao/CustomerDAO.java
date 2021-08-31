78ae4b0ea8783d842a121d9d759992c09c235c03
ahnjjun <88135979+ahnjjun@users.noreply.github.com>
2021-08-31 17:03:17 +0900

    customerDAO

--- src/team2/mvc/model/dao/CustomerDAO.java
+++ src/team2/mvc/model/dao/CustomerDAO.java
@@ -23,7 +23,9 @@ public interface CustomerDAO {
 
 	List<User> passwordUpdate(int user_nums, String pw1) throws SQLException;
 
-	void putTag(int movienum) throws Exception;
+	void putTag(int userNo, int movienum) throws Exception;
+
+	
 
 	
 	
